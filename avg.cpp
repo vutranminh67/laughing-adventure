496720b12db7d01adc01857ed63742a94abac7dd
Your Name <you@example.com>
2025-09-22 09:58:47 +0700

    update

---
+++ avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float grade1 = 85.5; 
+  float grade2 = 97.0;
+  float total, average;
+   total = grade1 + grade2;
+  average = total/2.0;  // divide the total by 2.0
+  cout << "The average grade is " << average << endl;
+  return 0;
+}
